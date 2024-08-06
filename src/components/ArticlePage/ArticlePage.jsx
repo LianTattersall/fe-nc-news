29b453af6d60e7dd87fe88db72c884ba4bb149ca
Lian Tattersall <liantattersall@Lians-iMac.lan>
2024-08-06 11:34:58 +0100

    add an article page component

--- src/components/ArticlePage/ArticlePage.jsx
+++ src/components/ArticlePage/ArticlePage.jsx
@@ -1,12 +1,14 @@
 import { useParams } from "react-router-dom";
 import ArticleSection from "./ArticleSection";
+import CommentSection from "./CommentsSection";
 
 function ArticlePage() {
   const { article_id } = useParams();
-  console.log("hello");
+
   return (
     <main style={{ marginTop: "50px" }}>
       <ArticleSection article_id={article_id} />
+      <CommentSection article_id={article_id} />
     </main>
   );
 }
