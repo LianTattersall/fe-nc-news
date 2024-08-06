e1f4b2433b24941d66cb7f37d0148de3a3bfe939
Lian Tattersall <liantattersall@Lians-iMac.lan>
2024-08-06 11:35:44 +0100

    add changes to article section

--- src/components/ArticlePage/ArticleSection.jsx
+++ src/components/ArticlePage/ArticleSection.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { getArticleById } from "../../api";
 import { formatDate } from "../../../utils/formatDate";
+import "./ArticleSection.css";
 
 function ArticleSection({ article_id }) {
   const [article, setArticle] = useState({});
@@ -9,19 +10,16 @@ function ArticleSection({ article_id }) {
       setArticle(data.article);
     });
   }, []);
-  console.log(article_id);
 
   return (
     <section className="article-section">
       <h2>{article.title}</h2>
       <img src={article.article_img_url} alt="article img" />
-      <p>
-        <span>Auhtor:</span> {article.author}
+      <p className="date">
+        {article.created_at ? ` ${formatDate(article.created_at)}` : null}
       </p>
-      <p>
-        {article.created_at ? `Date: ${formatDate(article.created_at)}` : null}
-      </p>
-      <p>{article.body}</p>
+      <p className="author">By {article.author}</p>
+      <p className="body">{article.body}</p>
     </section>
   );
 }
