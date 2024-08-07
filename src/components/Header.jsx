c576aa3b32518a94a746b3bb0d4f24a5c90ff1f7
Lian Tattersall <liantattersall@Lians-iMac.lan>
2024-08-07 14:03:45 +0100

    add changes to header to include all categories

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,20 +2,35 @@ import * as React from "react";
 import MenuIcon from "@mui/icons-material/Menu";
 import { Box, Drawer, List, ListItem } from "@mui/material";
 import { Link } from "react-router-dom";
+import { getTopics } from "../api";
 
 function Header() {
   const [open, setOpen] = React.useState(false);
+  const [topics, setTopics] = React.useState([]);
 
   const toggleDrawer = (newOpen) => () => {
     setOpen(newOpen);
   };
 
+  React.useEffect(() => {
+    getTopics().then((data) => {
+      setTopics(data.topics);
+    });
+  }, []);
+
   const DrawerList = (
     <Box sx={{ width: 250 }} role="presentation" onClick={toggleDrawer(false)}>
       <List>
-        <Link to={`/topics/All`}>
+        <Link to={`/topics`} key={"all"}>
           <ListItem>All Articles</ListItem>
         </Link>
+        {topics.map((topic) => {
+          return (
+            <Link to={`topics/${topic.slug}`} key={topic.slug}>
+              <ListItem>{topic.slug}</ListItem>
+            </Link>
+          );
+        })}
       </List>
     </Box>
   );
