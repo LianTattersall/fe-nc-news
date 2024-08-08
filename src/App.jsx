f7b427f2c4fc48a48ba46ea3d3ac6cab1bcf74f4
Lian Tattersall <liantattersall@Lians-iMac.lan>
2024-08-08 09:08:11 +0100

    add endpoint for all other paths

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,17 @@ import "./App.css";
 import Header from "./components/Header";
 import TopicPage from "./components/TopicPage/TopicPage";
 import ArticlePage from "./components/ArticlePage/ArticlePage";
+import PageNotFound from "./components/Errors/PageNotFound";
 
 function App() {
   return (
     <>
       <Header />
       <Routes>
+        <Route path="/" />
         <Route path="/topics/:slug?" element={<TopicPage />} />
         <Route path="/articles/:article_id" element={<ArticlePage />} />
+        <Route path="*" element={<PageNotFound />} />
       </Routes>
     </>
   );
